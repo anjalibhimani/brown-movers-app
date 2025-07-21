d079b91cf1cb7c9ce75ed8ecd9c83288bf7e4241
anjalibhimani <anjali.bhimani0@gmail.com>
2025-07-21 22:25:10 +0200

    sign up page

--- backend/app.py
+++ backend/app.py
@@ -6,7 +6,7 @@ import os
 def create_app():
     app = Flask(__name__)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://user:password@localhost:5432/brown_moving_app')
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql://anbhimani:Radhu-176-$jlm#@localhost:5432/brown_moving_app')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_super_secret_jwt_key')
 
