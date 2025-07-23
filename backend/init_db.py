f2d42a27db68f0800b76c3ea6541e92c9d5c9b71
Anjali Bhimani <anjali.bhimani0@gmail.com>
2025-07-23 22:32:05 +0200

    delete .env and init_db.py

--- backend/init_db.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-from extensions import db
-
-app = create_app()
-
-with app.app_context():
-    print("Creating database tables...")
-    db.create_all()
-    print("Database tables created.")
