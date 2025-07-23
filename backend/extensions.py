b0291bb9b615f6c1ac97b1fb587a786fd531f652
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-23 22:26:16 +0200

    commented extensions.py

--- backend/extensions.py
+++ backend/extensions.py
@@ -1,9 +1,18 @@
+# initializes all the required shared Flask extensions like the database and JSON Web Token for backend files
+
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
 
+# to interact with database using python
 db = SQLAlchemy()
+
+# to hash passwords
 bcrypt = Bcrypt()
+
+# for authorization of routes
 jwt = JWTManager()
+
+# to allow and restrict frontend to make requests to backend on different domains 
 cors = CORS()
