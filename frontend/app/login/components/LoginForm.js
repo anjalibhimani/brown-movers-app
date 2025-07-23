4cae54e691f0ced42724731513fdd3fa529091de
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-23 23:28:58 +0200

    commented LoginForm.js

--- frontend/app/login/components/LoginForm.js
+++ frontend/app/login/components/LoginForm.js
@@ -1,17 +1,38 @@
+// provides form with email and password fields for logging in
+
 'use client';
 import { useState } from 'react';
 
-export default function LoginForm({ onSubmit, error, loading }) {
+/**
+ * form component for when user logs in 
+ *
+ * @param {function({email: string, password: string}): void} props.onLogin - callback function to run when form is submitted
+ * @param {string} props.error - an error message to put on the form
+ * @param {boolean} props.loading - flag indicating if the form is currently submitting or not
+ *
+ * @returns {JSX.Element} - rendered login form
+ */
+
+export default function LoginForm({ onLogin, error, loading }) {
+  // states for the email and password user input fields
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+   /**
+   * deals with the submission of the login form by the user
+   *
+   * @param {React.FormEvent<HTMLFormElement>} e - the form submission event when submitted by user 
+   */
+  
+  function handleSubmit(e) {
+    // block browser's default reloading, and call the onLogin prop function 
     e.preventDefault();
-    onSubmit({ email, password });
-  };
+    onLogin({ email, password });
+  }
 
   return (
     <form onSubmit={handleSubmit}>
+      {/* email and password input fields for user, with types to ensure emails and passwords are being entered */}
       <div>
         <label htmlFor="email">Email:</label>
         <input 
@@ -32,7 +53,9 @@ export default function LoginForm({ onSubmit, error, loading }) {
           required 
         />
       </div>
+      {/* display potential login error message */}
       {error && <p style={{ color: 'red' }}>{error}</p>}
+      {/* disable submit button while system processing the login request */}
       <button type="submit" disabled={loading}>
         {loading ? 'Logging In...' : 'Login'}
       </button>
