869ad5e20f3e8131be4aac22e10e40dedb488ac8
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-23 14:57:14 +0200

    commented and added database creation in app.py

--- backend/app.py
+++ backend/app.py
@@ -23,22 +23,33 @@ def create_app():
     # turn off tracking
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-    # 
+    # configure key to sign and authenticate JSON Web Tokens 
     app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY', 'your_super_secret_jwt_key')
 
+    # initialize the databse, encryption, and JSON Web Token extensions for the Flask app 
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    # enable frontend server to make requests 
+    cors = CORS(resources={r"/api/*": {"origins": "*"}})
     cors.init_app(app)
 
+    # modularize app for organization purposes, all authorization routes prefixed to /api/auth
     app.register_blueprint(auth_bp, url_prefix='/api/auth')
 
+    # confirm upon running to check if API is up and functioning
     @app.route('/')
     def index():
         return jsonify({"message": "Welcome to Brown Moving App API!"})
 
+    # create all uncreated tables from defined models 
+    with app.app_context():
+        db.create_all()
+        
     return app
 
+# create the app, and run on debug mode to help with dev
 if __name__ == '__main__':
     app = create_app()
     app.run(debug=True)
