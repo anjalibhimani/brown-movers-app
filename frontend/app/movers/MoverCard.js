5c6f09b9a3f5a035c9d42bced242c6bd8307247b
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-24 10:50:45 +0200

    commented MoverCard.js

--- frontend/app/movers/MoverCard.js
+++ frontend/app/movers/MoverCard.js
@@ -1,7 +1,17 @@
+// renders summary card for a mover
+
 import Link from 'next/link';
+
+/**
+ * reusable UI component that to display sumamry info for a mover, card is a link to take user to more info page 
+ *
+ * @param {object} props.mover - mover object with details like the id, name, profile pic, etc 
+ * @returns {JSX.Element} - linkable card for the mover's profile 
+ */
 
 export default function MoverCard({ mover }) {
   return (
+    // reference link to page based on the mover's id
     <Link href={`/movers/${mover.id}`} passHref>
       <div
         style={{
@@ -16,6 +26,7 @@ export default function MoverCard({ mover }) {
           color: 'inherit'
         }}
       >
+      {/* if file/pic exists, then render the pic else just have a placeholder*/}
         {mover.profile_picture ? (
           <img
             src={`/api/auth/uploads/${mover.profile_picture}`}
@@ -25,6 +36,7 @@ export default function MoverCard({ mover }) {
         ) : (
           <div style={{ width: 80, height: 80, borderRadius: "50%", background: "#eee", margin: "0 auto 8px" }} />
         )}
+        {/* show mover's full name and grad year */}
         <div style={{ fontWeight: 600, fontSize: 18 }}>
           {mover.first_name} {mover.last_name}
         </div>
