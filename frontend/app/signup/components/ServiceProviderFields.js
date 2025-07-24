0b1e6a3988f797f321c8e6f95240913df3bae63f
Anjali Bhimani <109691875+anjalibhimani@users.noreply.github.com>
2025-07-24 12:13:27 +0200

    additional features in ServiceProviderFields.js
    - commented
    - added more options for services
    - can checkbox whatever service they want

--- frontend/app/signup/components/ServiceProviderFields.js
+++ frontend/app/signup/components/ServiceProviderFields.js
@@ -1,19 +1,40 @@
+// renders additional mover fields 
+
+/**
+ * Component that renders additional form fields for user who wants to be a mover 
+ *
+ * @param {object} props.formData - current state of the form data
+ * @param {function(React.ChangeEvent<HTMLInputElement>): void} props.handleChange - generic change handler for inputs
+ * @param {function(React.ChangeEvent<HTMLInputElement>): void} props.handleServiceChange - specific handler for checkboxes
+ * @param {function(React.ChangeEvent<HTMLInputElement>): void} props.handleFileChange - specific handler for file upload
+ * 
+ * @returns {JSX.Element} - fragment with mover fields
+ */
+
 export default function ServiceProviderFields({ formData, handleChange, handleServiceChange, handleFileChange }) {
+  // list of available services
+  const services = ["Moving", "Packing", "Furniture Assembly", "Storage"];
+  
   return (
     <div>
       <h3>Service Provider Details</h3>
       <div>
         <label>Services Offered:</label>
-        <div>
-          <label>
-            <input type="checkbox" name="servicesOffered" value="Move Boxes" checked={formData.servicesOffered.includes('Move Boxes')} onChange={handleServiceChange} /> Move Boxes
-          </label>
-        </div>
-        <div>
-          <label>
-            <input type="checkbox" name="servicesOffered" value="Help Pack" checked={formData.servicesOffered.includes('Help Pack')} onChange={handleServiceChange} /> Help Pack
-          </label>
-        </div>
+        {/* map over services array to create a checkbox for each */}
+        {services.map(service => (
+          <div key={service}>
+            <input
+              type="checkbox"
+              id={service}
+              name="servicesOffered"
+              value={service}
+              // Check the box if the service is in the formData's servicesOffered array.
+              checked={formData.servicesOffered.includes(service)}
+              onChange={handleServiceChange}
+            />
+            <label htmlFor={service}>{service}</label>
+          </div>
+        ))}
       </div>
       <div>
         <label htmlFor="hourlyRate">Hourly Rate ($):</label>
